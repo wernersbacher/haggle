920ce449ff5419850e00c446bed64e323737c225
Wernersbach, Markus <wernersbacher@gmail.com>
2024-10-27 19:22:15 +0100

    fix workflow and test

--- src/app/services/game.service.spec.ts
+++ src/app/services/game.service.spec.ts
@@ -1,7 +1,14 @@
 import { GameService } from './game.service';
+import { StateService } from './state-service';
 
 describe('GameService', () => {
-  let service: GameService = new GameService();
+  let stateService: StateService;
+  let service: GameService;
+
+  beforeEach(() => {
+    stateService = new StateService();
+    service = new GameService(stateService);
+  });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
